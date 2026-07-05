125f317721be8caea2e6107ba2bee0569f95fc65
Aditya Subramanian <adu101992@gmail.com>
2026-07-05 17:15:10 +0530

    - Adding the level scene and its generation. - Adding the ship along with rigidbody for applying impulse. - Moving ship left and right. - Trying to fix why character forward vector not being the right one!

---
+++ asteroids/TSCN/level_gen.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+const LEVEL_SCENE = preload("res://TSCN/level.tscn")
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	_load_level()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+func _load_level() -> void:
+	var level_instance = LEVEL_SCENE.instantiate()
+	level_instance.global_position.x = 0.0
+	level_instance.global_position.y = 0.0
+	add_child(level_instance)
---
+++ asteroids/TSCN/ship.gd
@@ -0,0 +1,34 @@
+extends Node2D
+
+var forward_vector = -global_transform.y
+
+@export var rotationRadian : float = 0.0
+var rb2D : RigidBody2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	#1 degree in radians * 10.
+	rotationRadian = 0.174533
+	rb2D = get_child(1) as RigidBody2D
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+func _input(_event: InputEvent) -> void:
+	#rotate left
+	if _event is InputEventKey && _event.keycode == KEY_D:
+		if _event.is_pressed():
+			rotate(rotationRadian)
+	#rotate right
+	if _event is InputEventKey && _event.keycode == KEY_A:
+		if _event.is_pressed():
+			rotate(-rotationRadian)
+	#propel forwards
+	if _event is InputEventKey && _event.keycode == KEY_W:
+		if _event.is_pressed():
+			rb2D.apply_impulse(forward_vector)
+	#propel backwards
+	if _event is InputEventKey && _event.keycode == KEY_S:
+		if _event.is_pressed():
+			rb2D.apply_impulse(-forward_vector)
