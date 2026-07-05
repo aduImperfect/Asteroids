4f7890390a2a1104a1e5e3d62e8ce208deea8304
Aditya Subramanian <adu101992@gmail.com>
2026-07-05 17:58:50 +0530

    Ship rotates and moves but while moving, rotation stops!

--- asteroids/TSCN/ship.gd
+++ asteroids/TSCN/ship.gd
@@ -1,19 +1,22 @@
-extends Node2D
+extends RigidBody2D
+#extends Node2D
 
-var forward_vector = -global_transform.y
-
+@export var forward_vector = -global_transform.y
 @export var rotationRadian : float = 0.0
-var rb2D : RigidBody2D
+#var rb2D : RigidBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	#1 degree in radians * 10.
 	rotationRadian = 0.174533
-	rb2D = get_child(1) as RigidBody2D
+	#rb2D = get_child(1) as RigidBody2D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
+
+func _physics_process(_delta: float) -> void:
+	forward_vector = -global_transform.y
 
 func _input(_event: InputEvent) -> void:
 	#rotate left
@@ -27,8 +30,8 @@ func _input(_event: InputEvent) -> void:
 	#propel forwards
 	if _event is InputEventKey && _event.keycode == KEY_W:
 		if _event.is_pressed():
-			rb2D.apply_impulse(forward_vector)
+			apply_impulse(forward_vector)
 	#propel backwards
 	if _event is InputEventKey && _event.keycode == KEY_S:
 		if _event.is_pressed():
-			rb2D.apply_impulse(-forward_vector)
+			apply_impulse(-forward_vector)
