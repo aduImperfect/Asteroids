76d8d19d4b6d6e0ef19c5be5c640e7d2b9b3a22b
Aditya Subramanian <adu101992@gmail.com>
2026-07-05 20:18:05 +0530

    Varying sizes for the asteroid

--- asteroids/TSCN/asteroid.gd
+++ asteroids/TSCN/asteroid.gd
@@ -8,31 +8,36 @@ extends RigidBody2D
 @export var firstRotation : float = 0.0
 @export var infiniteMax : Vector2
 
+@export var size : int = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	size = randi_range(2, 10)
 	infiniteMax = Vector2(-9000.0, -9000.0)
 	#1 degree in radians.
 	rotationRadian = 0.0174533
 	directionRight = randi_range(0, 10)
 	#Keeping value at 1/10 as max range and multiplying to get lesser range of angles for each asteroid!
 	firstRotation = randf_range(0.0, 0.628319) * 10.0
+	rotate(firstRotation)
 	#Apply force on spawning!
-	force_range = randf_range(5.0, 20.0)
+	force_range = randf_range(5.0, 10.0)
 	#apply_force(forward_vector * force_range)
+	get_child(0).scale *= size
+	get_child(1).scale *= size
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	var tempPos : Vector2 = ScreenWrapperHelper._get_new_position(position)
 	#Do not reset position if already on same position to make sure the physics code does not get overwritten!!
-	if tempPos.x < infiniteMax.x && tempPos.y < infiniteMax.y:
+	if tempPos.x < infiniteMax.x || tempPos.y < infiniteMax.y:
 		return
 	#Update to new position based on screen wrapping!
 	position = tempPos
-	pass
 
 func _physics_process(_delta: float) -> void:
 	apply_force(forward_vector * force_range)
-	if directionRight < 5:
-		rotate(rotationRadian)
-	else:
-		rotate(-rotationRadian)
+	#if directionRight < 5:
+		#rotate(rotationRadian)
+	#else:
+		#rotate(-rotationRadian)
